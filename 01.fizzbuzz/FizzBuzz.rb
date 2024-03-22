ec740048122572d8afd7b5f21f49bfd920717228
mytkyun <sim.rb.spy@gmail.com>
2024-03-22 21:40:15 +0900

    revision 3.22

--- 01.fizzbuzz/FizzBuzz.rb
+++ 01.fizzbuzz/FizzBuzz.rb
@@ -1,16 +1,10 @@
-# 1-20を表示する
-numbers = 1 .. 20
-numbers.each do |number|
-  # 3と5の倍数のときFizzBuzz
+(1..20).each do |number|
   if number % 15 == 0 
     puts "FizzBuzz"
-  # 3の倍数のときFizz
   elsif number % 3 == 0
     puts "Fizz"
-    # 5の倍数のときBuzz
   elsif number % 5 == 0
     puts "Buzz"
-  # それ以外のもの
   else
     puts number
   end
