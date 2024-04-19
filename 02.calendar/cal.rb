f5aefe5d5d0689b53f74afb882a1659f9fca3049
mytkyun <sim.rb.spy@gmail.com>
2024-04-19 21:21:24 +0900

    日時指定・日付表示の修正

--- 02.calendar/cal.rb
+++ 02.calendar/cal.rb
@@ -2,30 +2,24 @@
 require "date"
 require "optparse"
 
-# 今日の日付を取得
+# 今日の日付を取得(デフォルト値の設定)
 today = Date.today
-today_year = today.year
-today_month = today.month
+year = today.year
+month = today.month
 
-# コマンドラインで入力された内容をもとに年月を指定
+# コマンドラインで指定があった場合の分岐
 params = ARGV.getopts("y:", "m:")
 
-if params["y"] == nil
-  year = today_year
-else 
+if params["y"] != nil 
   year = params["y"].to_i
 end
 
-if params["m"] == nil
-  month = today_month
-else 
+if params["m"] != nil
   month = params["m"].to_i
 end
 
-# 月のはじまりと終わり
-start_of_month = Date.new(year, month, 1).day
-end_of_month = Date.new(year, month, -1).day
-# 月の初日の曜日を取得する
+# 月の日数の取得、初日の曜日取得
+days = Date.new(year, month, -1).day
 weekday_1st = Date.new(year, month, 1).wday
 
 # ヘッダー
@@ -33,20 +27,16 @@ puts "#{month}月 #{year}".center(20)
 puts "日 月 火 水 木 金 土"
 
 # 1週目の空白を表示する
-weekday_1st.times do
-  print "   "
-end
+print "   " * weekday_1st
 
-
-# 2週目以降の日付表示
-end_of_month.times do |n|
-  break if n == end_of_month
-  if n + 1  == 0
-    printf("%2d ", n + 1)
-  elsif (n + 1) % 7 == 6
-    printf("%2d ", n + 1)
+# 日付を表示
+(1..days).each do |n|
+  if n == 0
+    printf("%2d ", n)
+  elsif n % 7 == 6
+    printf("%2d ", n)
     puts ""
   else
-    printf("%2d ", n + 1)
+    printf("%2d ", n)
   end
 end
