6c5edb965294bad4c5b4507164cf071dd5c0d7db
mytkyun <sim.rb.spy@gmail.com>
2024-12-10 21:30:55 +0900

    ファイル名の表示幅を文字数を取得して設定するようにした

--- 04.ls/ls-command.rb
+++ 04.ls/ls-command.rb
@@ -1,21 +1,31 @@
-filenames = []
-Dir.foreach(Dir.getwd) do |filename|
-  if filename =~ /^\./
-  else
-    filenames << filename
+def filenames
+  filenames = []
+  Dir.foreach(Dir.getwd) do |filename|
+    if filename =~ /^\./
+    else
+      filenames << filename
+    end
+  end
+  filenames.sort!
+end
+
+def filenames_row
+  if filenames.length.to_i % 3 == 0
+    filenames.length/3.to_i
+  else 
+    filenames.length/3.to_i + 1
   end
 end
 
-if filenames.length.to_i % 3 == 0
-  filenames_row = filenames.length/3.to_i
-else 
-  filenames_row = filenames.length/3.to_i + 1
-end
-
-filenames_row.times do |n|
-  if filenames[filenames_row * 2 + n] == nil
-    puts filenames[n].ljust(15) + filenames[filenames_row + n].ljust(15)
-  else
-    puts filenames[n].ljust(15) + filenames[filenames_row + n].ljust(15) + filenames[filenames_row * 2 + n].ljust(15) 
+def view_filenames
+  width = filenames.sort_by {|array| array.length}.reverse[0].length + 5
+  filenames_row.times do |n|
+    if filenames[filenames_row * 2 + n] == nil
+      puts filenames[n].ljust(width) + filenames[filenames_row + n].ljust(width)
+    else
+      puts filenames[n].ljust(width) + filenames[filenames_row + n].ljust(width) + filenames[filenames_row * 2 + n].ljust(width) 
+    end
   end
 end
+
+view_filenames
