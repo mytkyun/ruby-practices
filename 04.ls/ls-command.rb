9534ad240dd3b077e42d753c362647366d2f33eb
mytkyun <sim.rb.spy@gmail.com>
2024-12-15 16:44:14 +0900

    必要最小限の変更で行数の変更に対応できるように、表示部分を修正

--- 04.ls/ls-command.rb
+++ 04.ls/ls-command.rb
@@ -1,31 +1,31 @@
-def filenames
+def get_sorted_filenames
   filenames = []
   Dir.foreach(Dir.getwd) do |filename|
-    if filename =~ /^\./
-    else
-      filenames << filename
-    end
+    next if filename =~ /^\./
+    filenames << filename
   end
   filenames.sort!
 end
 
-def filenames_row
-  if filenames.length.to_i % 3 == 0
-    filenames.length/3.to_i
-  else 
-    filenames.length/3.to_i + 1
-  end
+def calc_row(length, col)
+  length / col + (length % col == 0 ? 0 : 1)
 end
 
-def view_filenames
-  width = filenames.sort_by {|array| array.length}.reverse[0].length + 5
-  filenames_row.times do |n|
-    if filenames[filenames_row * 2 + n] == nil
-      puts filenames[n].ljust(width) + filenames[filenames_row + n].ljust(width)
-    else
-      puts filenames[n].ljust(width) + filenames[filenames_row + n].ljust(width) + filenames[filenames_row * 2 + n].ljust(width) 
+def calc_width(names)
+  names.sort_by {|name| name.length}.reverse[0].length + 5
+end
+
+def view_filename(col)
+  filenames = get_sorted_filenames
+  width = calc_width(filenames)
+  row = calc_row(filenames.length, col)
+  row.times do |n|
+    col.times do |x|
+      next if filenames[row * x + n] == nil
+      print filenames[row * x + n].ljust(width) 
     end
+    puts
   end
 end
 
-view_filenames
+view_filename(3)
