46a78741b79b1f725ebcf4c01c9485b846452789
mytkyun <sim.rb.spy@gmail.com>
2024-12-16 20:39:41 +0900

    rubocopの修正 一時保存

---
+++ 04.ls/ls_command.rb
@@ -0,0 +1,35 @@
+# frozen_string_literal: true
+
+def sorted_filenames
+  filenames = []
+  Dir.foreach(Dir.getwd) do |filename|
+    next if filename =~ /^\./
+
+    filenames << filename
+  end
+  filenames.sort!
+end
+
+def calc_row(length, col)
+  length / col + (length % col == zero ? 0 : 1)
+end
+
+def calc_width(names)
+  names.sort_by { |name| name.length }.reverse[0].length + 5
+end
+
+def view_filename(col)
+  filenames = sorted_filenames
+  width = calc_width(filenames)
+  row = calc_row(filenames.length, col)
+  row.times do |n|
+    col.times do |x|
+      next if filenames[row * x + n] <= filenames.length
+
+      print filenames[row * x + n].ljust(width)
+    end
+    puts
+  end
+end
+
+view_filename(3)
