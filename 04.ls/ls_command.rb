a1b2eca9cae2fb8496cb988049dfb6966562918e
mytkyun <sim.rb.spy@gmail.com>
2025-06-25 15:55:45 +0900

    ls -lオプション構成やり直し

--- 04.ls/ls_command.rb
+++ 04.ls/ls_command.rb
@@ -20,85 +20,88 @@ def sorted_filenames(options)
   filenames
 end
 
-def l_option
-  options = fetch_options
-  return unless options[:l]
+options = fetch_options
+return unless options[:l]
 
-  filenames = sorted_filenames(options)
-  filetype = { '01' => 'p',  '02' => 'c', '04' => 'd', '06' => 'b', '10' => '-', '12' => 'l', '14' => 's' }
-  filemode = { '0' => '---', '1' => '--x', '2' => '-w-', '3' => '-wx', '4' => 'r--', '5' => 'r-x', '6' => 'rw-', '7' => 'rwx' }
-  permissions = []
-  nlinks = []
-  user_ids = []
-  group_ids = []
-  sizes = []
-  mtimes = []
-  blocks = []
+filetypes = { '01' => 'p',  '02' => 'c', '04' => 'd', '06' => 'b', '10' => '-', '12' => 'l', '14' => 's' }
+filemodes = { '0' => '---', '1' => '--x', '2' => '-w-', '3' => '-wx', '4' => 'r--', '5' => 'r-x', '6' => 'rw-', '7' => 'rwx' }
 
-  filenames.each do |n|
-    stat = File.stat(n)
-    stat_mode = stat.mode.to_s(8)
-    stat_mode = "0 #{stat_mode}" if stat_mode.size == 5
-    user_permissions = filemode[stat_mode.slice(3)]
-    if user_permissions.slice(2) == 'x' && filemode[stat_mode.slice(3)] == '1'
-      user_permissions[2] = 't'
-    elsif user_permissions.slice(2) == '-' && filemode[stat_mode.slice(3)] == '1'
-      user_permissions[2] = 'T'
-    end
-
-    group_permissions = filemode[stat_mode.slice(4)]
-    if group_permissions.slice(2) == 'x' && filemode[stat_mode.slice(3)] == '2'
-      group_permissions[2] = 's'
-    elsif group_permissions.slice(2) == '-' && filemode[stat_mode.slice(3)] == '2'
-      group_permissions[2] = 'S'
-    end
-
-    other_permissions = filemode[stat_mode.slice(5)]
-    if other_permissions.slice(2) == 'x' && filemode[stat_mode.slice(3)] == '4'
-      other_permissions[2] = 's'
-    elsif other_permissions.slice(2) == '-' && filemode[stat_mode.slice(3)] == '4'
-      other_permissions[2] = 'S'
-    end
-
-    perimission = filetype[stat_mode.slice(0..1)] + user_permissions + group_permissions + other_permissions
-    permissions << perimission
-
-    nlinks << stat.nlink
-
-    user_ids << Etc.getpwuid(stat.uid).name
-
-    group_ids << Etc.getgrgid(stat.gid).name
-
-    sizes << stat.size
-
-    mtime = stat.mtime
-    mtimes << mtime.strftime('%_m %_d %R')
-
-    blocks << stat.blocks
+def user_permission(filemodes, stat_mode)
+  user_permission = filemodes[stat_mode.slice(3)]
+  if user_permission.slice(2) == 'x' && filemodes[stat_mode.slice(3)] == '1'
+    user_permission[2] = 't'
+  elsif user_permission.slice(2) == '-' && filemodes[stat_mode.slice(3)] == '1'
+    user_permission[2] = 'T'
   end
-
-  puts "total #{blocks.sum}"
-
-  nlinks_to_i = nlinks.map(&:to_s)
-
-  nlinks_blank = nlinks_to_i.max_by(&:length).length + 1
-  user_ids_blank = user_ids.max_by(&:length).length + 1
-  group_ids_blank = group_ids.max_by(&:length).length + 1
-
-  sizes_to_i = sizes.map(&:to_s)
-
-  sizes_blank = sizes_to_i.max_by(&:length).length + 1
-
-  filenames.each_index.select do |x|
-    print permissions[x], ' '
-    print nlinks[x].to_s.rjust(nlinks_blank), ' '
-    print user_ids[x].ljust(user_ids_blank)
-    print group_ids[x].ljust(group_ids_blank)
-    print sizes[x].to_s.rjust(sizes_blank), ' '
-    print mtimes[x], ' '
-    print filenames[x]
-    puts
-  end
+  user_permission
 end
 
-l_option
+def group_permission(filemodes, stat_mode)
+  group_permission = filemodes[stat_mode.slice(4)]
+  if group_permission.slice(2) == 'x' && filemodes[stat_mode.slice(3)] == '2'
+    group_permission[2] = 's'
+  elsif group_permission.slice(2) == '-' && filemodes[stat_mode.slice(3)] == '2'
+    group_permission[2] = 'S'
+  end
+  group_permission
+end
+
+def other_permission(filemodes, stat_mode)
+  other_permission = filemodes[stat_mode.slice(5)]
+  if other_permission.slice(2) == 'x' && filemodes[stat_mode.slice(3)] == '4'
+    other_permission[2] = 's'
+  elsif other_permission.slice(2) == '-' && filemodes[stat_mode.slice(3)] == '4'
+    other_permission[2] = 'S'
+  end
+  other_permission
+end
+
+filenames = sorted_filenames(options)
+filestats = []
+blocks = []
+
+filenames.each do |filename|
+  filestat = []
+  stat = File.stat(filename)
+  stat_mode = stat.mode.to_s(8)
+  stat_mode = "0#{stat_mode}" if stat_mode.size == 5
+  filetype = filetypes[stat_mode.slice(0..1)]
+  user_perm = user_permission(filemodes, stat_mode)
+  group_perm = group_permission(filemodes, stat_mode)
+  other_perm = other_permission(filemodes, stat_mode)
+  permission = "#{filetype}#{user_perm}#{group_perm}#{other_perm}"
+  filestat << permission
+  filestat << stat.nlink.to_s
+  filestat << Etc.getpwuid(stat.uid).name
+  filestat << Etc.getgrgid(stat.gid).name
+  filestat << stat.size.to_s
+  mtime = stat.mtime
+  filestat << mtime.strftime('%_m %_d %R')
+  blocks << stat.blocks
+  filestats << filestat
+end
+
+blank_permission = 0
+blank_nlink = 0
+blank_uid = 0
+blank_gid = 0
+blank_filesize = 0
+
+filestats.each do |x|
+  blank_permission = x[0].length if x[0].length > blank_permission
+  blank_nlink = x[1].length if x[1].length > blank_nlink
+  blank_uid = x[2].length if x[2].length > blank_uid
+  blank_gid = x[3].length if x[3].length > blank_gid
+  blank_filesize = x[4].length if x[4].length > blank_filesize
+end
+
+puts "total #{blocks.sum}"
+filestats.each do |n|
+  print n[0], '  '
+  print n[1].rjust(blank_nlink), ' '
+  print n[2].ljust(blank_uid), '  '
+  print n[3].ljust(blank_gid), '  '
+  print n[4].rjust(blank_filesize), ' '
+  print n[5], ' '
+  puts filenames[filestats.index(n)]
+end
