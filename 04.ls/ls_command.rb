55a942bd3817e331e754d137465573d056304f5a
mytkyun <sim.rb.spy@gmail.com>
2025-07-04 22:21:04 +0900

    ファイルの種類と権限一覧の配列を定数にした

--- 04.ls/ls_command.rb
+++ 04.ls/ls_command.rb
@@ -24,10 +24,10 @@ end
 options = fetch_options
 return unless options[:l]
 
-filetypes = { '01' => 'p',  '02' => 'c', '04' => 'd', '06' => 'b', '10' => '-', '12' => 'l', '14' => 's' }
-filemodes = { '0' => '---', '1' => '--x', '2' => '-w-', '3' => '-wx', '4' => 'r--', '5' => 'r-x', '6' => 'rw-', '7' => 'rwx' }
+FILETYPES = { '01' => 'p',  '02' => 'c', '04' => 'd', '06' => 'b', '10' => '-', '12' => 'l', '14' => 's' }.freeze
+FILEMODES = { '0' => '---', '1' => '--x', '2' => '-w-', '3' => '-wx', '4' => 'r--', '5' => 'r-x', '6' => 'rw-', '7' => 'rwx' }.freeze
 
-def user_permission(filemodes, stat_mode)
+def user_permission(stat_mode, filemodes = FILEMODES)
   user_permission = filemodes[stat_mode.slice(3)]
   if user_permission.slice(2) == 'x' && filemodes[stat_mode.slice(3)] == '1'
     user_permission[2] = 't'
@@ -37,7 +37,7 @@ def user_permission(filemodes, stat_mode)
   user_permission
 end
 
-def group_permission(filemodes, stat_mode)
+def group_permission(stat_mode, filemodes = FILEMODES)
   group_permission = filemodes[stat_mode.slice(4)]
   if group_permission.slice(2) == 'x' && filemodes[stat_mode.slice(3)] == '2'
     group_permission[2] = 's'
@@ -47,7 +47,7 @@ def group_permission(filemodes, stat_mode)
   group_permission
 end
 
-def other_permission(filemodes, stat_mode)
+def other_permission(stat_mode, filemodes = FILEMODES)
   other_permission = filemodes[stat_mode.slice(5)]
   if other_permission.slice(2) == 'x' && filemodes[stat_mode.slice(3)] == '4'
     other_permission[2] = 's'
@@ -65,10 +65,10 @@ filenames.each do |filename|
   filestat = []
   stat = File.stat(filename)
   stat_mode = stat.mode.to_s(8).rjust(6, '0')
-  filetype = filetypes[stat_mode.slice(0..1)]
-  user_perm = user_permission(filemodes, stat_mode)
-  group_perm = group_permission(filemodes, stat_mode)
-  other_perm = other_permission(filemodes, stat_mode)
+  filetype = FILETYPES[stat_mode.slice(0..1)]
+  user_perm = user_permission(FILEMODES, stat_mode)
+  group_perm = group_permission(FILEMODES, stat_mode)
+  other_perm = other_permission(FILEMODES, stat_mode)
   permission = "#{filetype}#{user_perm}#{group_perm}#{other_perm}"
   filestat << permission
   filestat << stat.nlink.to_s
@@ -86,27 +86,27 @@ filenames.each do |filename|
   filestats << filestat
 end
 
-blank_permission = 0
-blank_nlink = 0
-blank_uid = 0
-blank_gid = 0
-blank_filesize = 0
+width_permission = 0
+width_nlink = 0
+width_uid = 0
+width_gid = 0
+width_filesize = 0
 
 filestats.each do |x|
-  blank_permission = x[0].length if x[0].length > blank_permission
-  blank_nlink = x[1].length if x[1].length > blank_nlink
-  blank_uid = x[2].length if x[2].length > blank_uid
-  blank_gid = x[3].length if x[3].length > blank_gid
-  blank_filesize = x[4].length if x[4].length > blank_filesize
+  width_permission = x[0].length if x[0].length > width_permission
+  width_nlink = x[1].length if x[1].length > width_nlink
+  width_uid = x[2].length if x[2].length > width_uid
+  width_gid = x[3].length if x[3].length > width_gid
+  width_filesize = x[4].length if x[4].length > width_filesize
 end
 
 puts "total #{blocks.sum}"
 filestats.each do |n|
   print n[0], '  '
-  print n[1].rjust(blank_nlink), ' '
-  print n[2].ljust(blank_uid), '  '
-  print n[3].ljust(blank_gid), '  '
-  print n[4].rjust(blank_filesize), ' '
+  print n[1].rjust(width_nlink), ' '
+  print n[2].ljust(width_uid), '  '
+  print n[3].ljust(width_gid), '  '
+  print n[4].rjust(width_filesize), ' '
   print n[5], ' '
   puts filenames[filestats.index(n)]
 end
